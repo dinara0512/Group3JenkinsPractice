f495d25ec13c1b02effee712db460a4c130f3d6b
Dina <515lincolnstreet@gmail.com>
2024-02-25 10:52:46 -0500

    123

--- src/test/java/com/loop/step_definitions/Hooks.java
+++ src/test/java/com/loop/step_definitions/Hooks.java
@@ -5,14 +5,12 @@ import com.loop.utilities.utilities.BrowserUtilities;
 import com.loop.utilities.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-//import io.cucumber.java.Scenario;
-import cucumber.api.Scenario;
+import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
@@ -69,7 +67,7 @@ public class Hooks {
         if (scenario.isFailed()) {
             // taking a screenshot
             final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot, "image/png");
+           // scenario.embed(screenshot, "image/png");
         }
         Driver.closeDriver();
     }
